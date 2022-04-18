89aeb0cb5a7aeb7fbde2eb48e03983c53233fb6b
amplil <aikotobahaaburakatabura@gmail.com>
2022-04-18 14:57:44 +0900

    test1

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+const Index=()=>{
+  return(
+    <div>
+      <h1 style={{
+        color: "blue",
+        letterSpacing: "20px"
+      }}>Hello World
+      </h1>
+    </div>
+  )
+}
+export default Index
